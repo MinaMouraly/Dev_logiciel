f19a4400c0f6d8cfb1066be11e6966856fb76d10
MinaMouraly <minamouraly28@gmail.com>
2023-03-07 11:29:47 +0100

    creation d'un menu et ajout d'utilisateur

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const chalk= require('chalk');//permet d'ajouter de la couleur 
-var prompt= require('prompt');
+//var prompt= require('prompt');
 const fs = require('fs');//require permet de lire un fichier
 const process = require('process');
 let rawdata = fs.readFileSync('users.json');
@@ -17,11 +17,15 @@ const readlineSync = require('readline-sync');//readline permet d'avoir une conv
     console.log(
         chalk.blue('Menu:\n'+
         '1= pays\n'+
-        '2= societe\n')
+        '2= societe\n'+
+        '3= ajout utilisateur\n')
     );
     
+  
+  function main(){
+
     //on demande le choix a l'utilisateur 
-    var choix= readlineSync.question('Choisissez 1 ou 2:');
+    var choix= readlineSync.question('Choisissez 1,2 ou 3:');
 
     if (choix === '1' || input =='country') {
         //on met que les country dans country
@@ -65,16 +69,68 @@ const readlineSync = require('readline-sync');//readline permet d'avoir une conv
         res.sort((a, b) => b.count - a.count);
         console.log(res);
     }
+    else if(choix=='3')
+    {
+        utilisateur();
+    }
+}
+
+  //fonction pour l'utilisateur 
+  function utilisateur()
+  {
+
+    const newUser = {
+
+        id: readlineSync.question('ID: '),
+  
+        email: '',
+  
+        first: readlineSync.question('Prénom: '),
+  
+        last: readlineSync.question('Nom: '),
+  
+        company: readlineSync.question('Société: '),
+  
+        created_at: new Date().toISOString(),
+  
+        country: readlineSync.question('Pays: ')
+  
+      };
+    
+       // Demande de l'adresse email à l'utilisateur, avec une boucle pour s'assurer qu'elle est valide
+
+    let email;
+
+    do {
+
+      email = readlineSync.question('Email: ');
+
+      if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+
+        console.log(chalk.red('Adresse email invalide. Veuillez saisir une adresse email valide.'));
+
+        email = undefined;
+
+      }
+
+    } while (!email);
+
+
+    // Ajout de l'adresse email valide à l'utilisateur et sauvegarde dans le fichier users.json
+
+    newUser.email = email;
+
+    users.push(newUser);
+
+    fs.writeFileSync('users.json', JSON.stringify(users));
+
+    console.log('Utilisateur ajouté avec succès !');
+  }
+
+  main();
+
    
-    prompt.start();//on commence la demande d'info a l'user
-    
-    prompt.get(['username','email' ],function(err,result){
-     
-        console.log('comande line:\n');
-        console.log('username:'+result.username);
-        console.log('email:'+result.email);
-    
-    });
+   
 
     
 
